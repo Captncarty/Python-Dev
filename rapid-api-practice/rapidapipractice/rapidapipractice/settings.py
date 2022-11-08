ee70a4abf1c6bb36efc092a4040ba706ddba7920
Kelvin Maccarthy <111019478+Captncarty@users.noreply.github.com>
2022-11-08 15:46:10 +0100

    Update settings.py

--- rapid-api-practice/rapidapipractice/rapidapipractice/settings.py
+++ rapid-api-practice/rapidapipractice/rapidapipractice/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--wk8mtd^8fl04^72r=)))*&msr6g0-03wwq%^0@9gs0%q=m9bh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
